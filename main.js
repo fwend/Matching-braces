19ab3d324233cb8ac55f65d89485fb9590469d30
fwend <josvanuden@gmail.com>
2018-06-17 12:36:38 +0200

    renamed result to prevBraces

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const MatchingBraces = { '(': ')', '{': '}', '[': ']' };
 
 function checkBraces(s) {
-    let result = [];
+    let prevBraces = [];
+    
     for (let i = 0; i < s.length; i++) {
 
         if (MatchingBraces[s[i]]) {
-            result.push(s[i]);
+            prevBraces.push(s[i]);
 
-        } else if (s[i] !== MatchingBraces[result.pop()]) {
+        } else if (s[i] !== MatchingBraces[prevBraces.pop()]) {
             return false;
         }
     }
-    return result.length === 0;
+    return prevBraces.length === 0;
 }
